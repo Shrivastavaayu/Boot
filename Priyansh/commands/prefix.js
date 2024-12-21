8d91176d00efd1c4785bede92d2a184154eca8d4
Shrivastavaayu <Ayushshrivastava37639@gmail.com>
2024-12-21 15:47:03 +0530

    Update prefix.js

--- Priyansh/commands/prefix.js
+++ Priyansh/commands/prefix.js
@@ -26,7 +26,7 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
 const prefix = threadSetting.PREFIX || global.config.PREFIX;
       if (data.PREFIX == null) {
         return out(`This Is My Prefix ⇉ [ ${prefix} ]\n💝🥀𝐎𝐖𝐍𝐄𝐑:- ☞💐𝐀𝐘𝐔𝐒𝐇 𝐒𝐇𝐑𝐈𝐕𝐀𝐒𝐓𝐀𝐕💐☜ 💫\n🖤𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 Ayu🖤\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝🤓:- ☞ https://www.facebook.com/profile.php?id=100051705871957&mibextid=ZbWKwL\n
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇`)
+👋For Any Kind Of Help Contact On Telegram  Username 👉 @ayush shrivastava😇`)
       }
       else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n💝🥀𝐎𝐖𝐍𝐄𝐑:- ☞💐𝐀𝐘𝐔𝐒𝐇 𝐒𝐇𝐑𝐈𝐕𝐀𝐒𝐓𝐀𝐕💐☜ 💫\n🖤𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 Ayu🖤\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝🤓:- ☞ https://www.facebook.com/profile.php?id=100051705871957&mibextid=ZbWKwL\n👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇' + data.PREFIX)
     }
